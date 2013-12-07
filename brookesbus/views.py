27e57056a72def658c0bacdf544cb5978b83184c
Charlie Denton <charlie@meshy.co.uk>
2013-12-07 16:25:53 +0000

    Deshonkify.

--- brookesbus/views.py
+++ brookesbus/views.py
@@ -45,9 +45,9 @@ class StopList(ListView):
 class StopDetail(ListView):
     model = Schedule
 
-    def get(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         self.stop = get_object_or_404(Stop, pk=kwargs['pk'])
-        return super(StopDetail, self).get()
+        return super(StopDetail, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         return super(StopDetail, self).get_context_data(stop=stop, **kwargs)
