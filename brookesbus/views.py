c6da745a1cec737e119706079c154d2eccaf15a6
Charlie Denton <charlie@meshy.co.uk>
2013-12-07 16:25:10 +0000

    Fix typo.

--- brookesbus/views.py
+++ brookesbus/views.py
@@ -47,7 +47,7 @@ class StopDetail(ListView):
 
     def get(self, *args, **kwargs):
         self.stop = get_object_or_404(Stop, pk=kwargs['pk'])
-        return super(StopScheduleList, self).get()
+        return super(StopDetail, self).get()
 
     def get_context_data(self, **kwargs):
         return super(StopDetail, self).get_context_data(stop=stop, **kwargs)
